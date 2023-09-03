9a63730c5e184bdffe7c9e739a718150a6c5a513
Tyler Duffus <tyled94@hotmail.com>
2023-09-03 23:09:23 +1000

    Added progress bar to live order cards

---
+++ Client/wwwroot/js/OrderCardProgress.js
@@ -0,0 +1,13 @@
+﻿function SetAllOrderCardProgress()
+{
+    let liveOrderCards = document.querySelectorAll("#liveOrders .card .progress-bar[data-time]");
+
+    if (liveOrderCards === null)
+        return;
+
+    liveOrderCards.forEach(e => {
+        let datatime = Date.parse(e.dataset.time);
+        let timeleft = (datatime - Date.now()) / 1000; // ms to s
+        e.style.animationDelay = `${timeleft}s`;
+    });
+}
