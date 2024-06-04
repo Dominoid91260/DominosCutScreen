202198a6bffcfbfbf1a0af5181d2dd067e68a735
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-04 10:48:39 +1000

    First order of the day and large timed orders now play the makeline sound.

--- Client/wwwroot/js/OvenAlert.js
+++ Client/wwwroot/js/OvenAlert.js
@@ -1,11 +1,11 @@
 let audio;
 
-function playOvenAlert(bShort)
+function playOvenAlert(file)
 {
     // If audio exists and is playing, do nothing
     if (audio != null && !audio.paused)
         return;
 
-    audio = new Audio("/audio/alarm" + (bShort ? "_short" : "") + ".wav");
+    audio = new Audio("/audio/" + file);
     audio.play();
 }
